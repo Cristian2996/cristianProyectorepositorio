bd14b1e76fef9360709be47b9c5021c09a3ac6bc
Cristian2996 <35310900+Cristian2996@users.noreply.github.com>
2018-01-18 00:53:21 -0500

    Add files via upload
    Actualizacion de dao

--- src/unach/trabajogithub/dao/IRoles.java
+++ src/unach/trabajogithub/dao/IRoles.java
@@ -6,6 +6,6 @@ import java.util.*;
 
 public interface IRoles {
 public int insertar (Roles roles) throws Exception;
-    public Roles obtener(String id_roles) throws Exception;
+    public Roles obtener(int id_roles) throws Exception;
     public ArrayList<Roles> obtener () throws Exception;       
 }
--- src/unach/trabajogithub/dao/IUsuarios.java
+++ src/unach/trabajogithub/dao/IUsuarios.java
@@ -5,6 +5,6 @@ import unach.trabajogithub.entidades.Usuarios;
 
 public interface IUsuarios {
     public int insertar (Usuarios usuarios) throws Exception;
-    public Usuarios obtener(int id_usuarios) throws Exception;
+    public Usuarios obtener(int id_usuario) throws Exception;
     public ArrayList<Usuarios> obtener () throws Exception;    
 }
